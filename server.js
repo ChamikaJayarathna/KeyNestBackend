f5dfdb0d3d4511d1b7873aafe95ed041f35bee1d
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-14 09:34:50 +0530

    connet mongodb database

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from "mongoose";
 import "dotenv/config";
 import authRoute from './routes/auth-route.js';
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect(process.env.DATABASE_URL)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch(err => {
+    console.log(err);
+})
 
 app.use(express.json());
 
