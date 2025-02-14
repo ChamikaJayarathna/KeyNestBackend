f791984ddf4412c761c26526e7055b720bd6116b
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-14 10:39:26 +0530

    create user register router

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,8 +1,53 @@
+import bcrypt from "bcrypt";
+import User from "../schema/User.js";
+
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
 export const register = async (req, res) => {
-}
+  const { username, email, password } = req.body;
 
-export const login = async (req, res) => {
-}
+  if (username.length < 5) {
+    return res
+      .status(403)
+      .json({ error: "Username must be at least 5 letters long" });
+  }
 
-export const logout = (req, res) => {
-}
+  if (!email.length) {
+    return res.status(403).json({ error: "Enter Email" });
+  }
+
+  if (!emailRegex.test(email)) {
+    return res.status(403).json({ error: "Email is invalid" });
+  }
+
+  if (!passwordRegex.test(password)) {
+    return res
+      .status(403)
+      .json({
+        error:
+          "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters",
+      });
+  }
+
+  const hashpassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashpassword,
+  });
+
+  await newUser
+    .save()
+    .then((user) => {
+      return res.status(200).json(user);
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+};
+
+export const login = async (req, res) => {};
+
+export const logout = (req, res) => {};
