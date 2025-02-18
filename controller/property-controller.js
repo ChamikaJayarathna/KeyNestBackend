1f01c204a4bc6c49cead203b530b7bd848556005
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-18 17:59:08 +0530

    create basic property create route

--- controller/property-controller.js
+++ controller/property-controller.js
@@ -1,3 +1,32 @@
+import Property from "../schema/Property.js";
+
 export const addPost = async (req, res) => {
-    const body = req.body;
+    const { title, images, address, description, price, city, bedroom, type, property, utilities, pet } = req.body;
+
+    try {
+
+        if((!title || !address || !description || !price || !city || !bedroom || !type || !property || !utilities || !pet)){
+            return res.status(400).json({ message: 'All required fields must be filled' });
+        }
+    
+        const newProperty = new Property({
+            title,
+            images,
+            address,
+            description,
+            price,
+            city,
+            bedroom,
+            type,
+            property,
+            utilities,
+            pet
+        });
+        const savedProperty = await newProperty.save();
+        res.status(201).json(savedProperty);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+    
+
 }; 
