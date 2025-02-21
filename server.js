8593dba78c07843f6ba9e38b44f67c72e53f809b
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-21 16:26:56 +0530

    update databse conncetion vl

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import DBConnection from './config/dbconnection.js';
 import authRoute from './routes/auth-route.js';
 import propertyRoute from './routes/property-route.js';
-import DBConnection from './config/dbconnection.js';
 
 const app = express();
 const PORT = 3000;
