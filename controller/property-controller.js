540e772de25dfbf5a9677541858a40f5f3ea0151
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-22 18:40:52 +0530

    create update property route

--- controller/property-controller.js
+++ controller/property-controller.js
@@ -84,3 +84,59 @@ export const addProperty = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
+
+export const updateProperty = async (req, res) => {
+  const { id } = req.params;
+  const {
+    title,
+    address,
+    description,
+    price,
+    city,
+    bedroom,
+    type,
+    property,
+    utilities,
+    pet,
+    images,
+  } = req.body;
+  let authorId = req.userId;
+
+  try {
+    if (!authorId) {
+      return res.status(401).json({ message: "User not authenticated" });
+    }
+
+    const existingProperty = await Property.findById(id);
+
+    if (!existingProperty) {
+      return res.status(404).json({ message: "Property not found" });
+    }
+
+    if (existingProperty.author.toString() !== authorId) {
+      return res.status(403).json({ message: "Unauthorized to update this property" });
+    }
+
+    const updatedProperty = await Property.findByIdAndUpdate(
+      id,
+      {
+        title,
+        images,
+        address,
+        description,
+        price,
+        city,
+        bedroom,
+        type,
+        property,
+        utilities,
+        pet,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedProperty);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
--- routes/property-route.js
+++ routes/property-route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addProperty, getAllProperties, getSingleProperty } from '../controller/property-controller.js';
+import { addProperty, getAllProperties, getSingleProperty, updateProperty } from '../controller/property-controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/get-all-property', verifyToken, getAllProperties);
 router.get('/get-single-property/:id', verifyToken, getSingleProperty);
 router.post('/create-property', verifyToken, addProperty);
+router.post('/update-property/:id', verifyToken, updateProperty);
 
 export default router;
