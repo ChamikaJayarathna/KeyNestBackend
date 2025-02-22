75d9e5917f108964722457d67533904cac9be027
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-22 15:03:58 +0530

    create get single property route

--- controller/property-controller.js
+++ controller/property-controller.js
@@ -2,45 +2,85 @@ import Property from "../schema/Property.js";
 
 export const getAllProperties = async (req, res) => {
   try {
-    const properties = await Property.find(); 
+    const properties = await Property.find();
     res.status(200).json(properties);
   } catch (error) {
-    res.status(500).json({ message: 'Server Error', error: error.message });
+    res.status(500).json({ message: "Server Error", error: error.message });
   }
-}
+};
+
+export const getSingleProperty = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const property = await Property.findById(id);
+
+    if (!property) {
+      return res.status(404).json({ message: "Property not found" });
+    }
+
+    res.status(200).json(property);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
 
 export const addProperty = async (req, res) => {
-    const { title, address, description, price, city, bedroom, type, property, utilities, pet, images } = req.body;
-    let authorId = req.userId;
-  
-    try {
-      if (!authorId) {
-        return res.status(401).json({ message: 'User not authenticated' });
-      }
-  
-      if ((!title || !address || !description || !price || !city || !bedroom || !type || !property || !utilities || !pet)) {
-        return res.status(400).json({ message: 'All required fields must be filled' });
-      }
-  
-      const newProperty = new Property({
-        author: authorId,
-        title,
-        images,
-        address,
-        description,
-        price,
-        city,
-        bedroom,
-        type,
-        property,
-        utilities,
-        pet
-      });
-  
-      const savedProperty = await newProperty.save();
-      res.status(201).json(savedProperty);
-    } catch (error) {
-      res.status(500).json({ message: 'Server Error', error: error.message });
+  const {
+    title,
+    address,
+    description,
+    price,
+    city,
+    bedroom,
+    type,
+    property,
+    utilities,
+    pet,
+    images,
+  } = req.body;
+  let authorId = req.userId;
+
+  try {
+    if (!authorId) {
+      return res.status(401).json({ message: "User not authenticated" });
     }
+
+    if (
+      !title ||
+      !address ||
+      !description ||
+      !price ||
+      !city ||
+      !bedroom ||
+      !type ||
+      !property ||
+      !utilities ||
+      !pet
+    ) {
+      return res
+        .status(400)
+        .json({ message: "All required fields must be filled" });
+    }
+
+    const newProperty = new Property({
+      author: authorId,
+      title,
+      images,
+      address,
+      description,
+      price,
+      city,
+      bedroom,
+      type,
+      property,
+      utilities,
+      pet,
+    });
+
+    const savedProperty = await newProperty.save();
+    res.status(201).json(savedProperty);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
 };
-  
--- routes/property-route.js
+++ routes/property-route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addProperty, getAllProperties } from '../controller/property-controller.js';
+import { addProperty, getAllProperties, getSingleProperty } from '../controller/property-controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/get-all-property', verifyToken, getAllProperties);
+router.get('/get-single-property/:id', verifyToken, getSingleProperty);
 router.post('/create-property', verifyToken, addProperty);
 
 export default router;
