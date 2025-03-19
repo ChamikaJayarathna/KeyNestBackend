ac284ba536a5b9aaee4a8e1849a51a969df374f9
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-19 17:06:56 +0530

    update the user schema

--- schema/User.js
+++ schema/User.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+let profile_imgs_name_list = ["Chase", "Aiden", "Ryker", "Christopher", "Aidan", "Leah", "George", "Valentina", "Jude", "Emery", "Wyatt", "Sawyer", "Alexander", "Mason", "Destiny", "Sara", "Brian", "Oliver", "Maria", "Adrian"]
+
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -13,6 +15,16 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+    },
+    role: {
+        type: String, 
+        default: "user"
+    },
+    profile_img:{
+        type: String,
+        default: () => {
+            return `https://api.dicebear.com/9.x/adventurer-neutral/svg?seed=${profile_imgs_name_list[Math.floor(Math.random() * profile_imgs_name_list.length)]}`
+        }
     }
 })
 
