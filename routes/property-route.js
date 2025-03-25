23de8275bc4c725dfc3cbf25250810ba4e9dd5ca
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-25 10:04:28 +0530

    Update the property route

--- routes/property-route.js
+++ routes/property-route.js
@@ -12,7 +12,7 @@ import verifyToken from "../middleware/verifyToken.js";
 const router = express.Router();
 
 router.get("/get-all-property", getAllProperties);
-router.get("/get-single-property/:id", verifyToken, getSingleProperty);
+router.get("/get-single-property/:id", getSingleProperty);
 router.post("/create-property", verifyToken, addProperty);
 router.post("/update-property/:id", verifyToken, updateProperty);
 router.post("/search-property", searchProperty);
