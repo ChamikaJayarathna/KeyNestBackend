070e9b6adbb8d06a54e50a59788f7b2eecae2d6a
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-07 10:40:42 +0530

    update ChatSchema.js

--- schema/Chat.js
+++ schema/Chat.js
@@ -1,26 +1,26 @@
 import mongoose, { Schema } from "mongoose";
 
 const chatSchema = new Schema({
-    users: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
-    userIDs: {
-        type: mongoose.Schema.Types.ObjectId,
-    },
-    seenBy: {
-        type: mongoose.Schema.Types.ObjectId 
-    },
-    messages: {
-        type: mongoose.Schema.Types.ObjectId,
-    },
-    lastMessage: {
-        type: String
-    },
-    createdAt: {
-        type: Date, 
-        default: Date.now
-    }
+  users: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  userIDs: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  seenBy: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  messages: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  lastMessage: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export default mongoose.model('chat', chatSchema);
+export default mongoose.model("chat", chatSchema);
