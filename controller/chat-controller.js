5c7565aa9bc80fe2b5c0e38642db40841a9a0e62
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-07 12:00:36 +0530

    create addChats route

--- controller/chat-controller.js
+++ controller/chat-controller.js
@@ -1,4 +1,5 @@
 import Chat from "../schema/Chat.js";
+import User from "../schema/User.js";
 
 export const getChats = async (req, res) => {
   const tokenUserId = req.userId;
@@ -32,5 +33,38 @@ export const getChats = async (req, res) => {
   } catch (err) {
     console.error("Error getting chats:", err);
     res.status(500).json({ message: "Failed to get chats!" });
+  }
+};
+
+export const addChat = async (req, res) => {
+  const tokenUserId = req.userId;
+  const receiverId = req.body.receiverId;
+
+  try {
+    const existingChat = await Chat.findOne({
+      userIDs: { $all: [tokenUserId, receiverId] },
+      userIDs: { $size: 2 },
+    });
+
+    if (existingChat) {
+      return res.status(200).json(existingChat);
+    }
+
+    const newChat = new Chat({
+      users: [tokenUserId, receiverId],
+      userIDs: [tokenUserId, receiverId],
+    });
+
+    await newChat.save();
+
+    await User.updateMany(
+      { _id: { $in: [tokenUserId, receiverId] } },
+      { $push: { chats: newChat._id, chatIDs: newChat._id } }
+    );
+
+    res.status(200).json(newChat);
+  } catch (err) {
+    console.error("Error adding chat:", err);
+    res.status(500).json({ message: "Failed to add chat!" });
   }
 };
--- routes/chat-route.js
+++ routes/chat-route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getChats } from "../controller/chat-controller.js";
+import { addChat, getChats } from "../controller/chat-controller.js";
 import verifyToken from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/get-chats", verifyToken, getChats);
+router.post("/add-chats", verifyToken, addChat);
 
 export default router;
