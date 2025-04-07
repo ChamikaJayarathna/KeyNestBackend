fb811d5867fdc862a8785c8f3fb7e6340d39fe92
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-07 12:16:51 +0530

    create add message route

---
+++ controller/message-controller.js
@@ -0,0 +1,37 @@
+import Message from "../schema/Message.js";
+import Chat from "../schema/Chat.js";
+
+export const addMessage = async (req, res) => {
+  const tokenUserId = req.userId;
+  const chatId = req.params.chatId;
+  const text = req.body.text;
+
+  try {
+    const chat = await Chat.findOne({
+      _id: chatId,
+      userIDs: tokenUserId,
+    });
+
+    if (!chat) {
+      return res.status(404).json({ message: "Chat not found!" });
+    }
+
+    const message = new Message({
+      text,
+      chatId,
+      userId: tokenUserId,
+    });
+
+    await message.save();
+
+    chat.messages.push(message._id);
+    chat.seenBy = [tokenUserId];
+    chat.lastMessage = text;
+    await chat.save();
+
+    res.status(200).json(message);
+  } catch (err) {
+    console.error("Error adding message:", err);
+    res.status(500).json({ message: "Failed to add message!" });
+  }
+};
--- routes/message-route.js
+++ routes/message-route.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { addMessage } from "../controller/message-controller.js";
+import verifyToken from "../middleware/verifyToken.js";
 
 const router = express.Router();
+
+router.post("/add-message/:chatId", verifyToken, addMessage);
 
 export default router;
